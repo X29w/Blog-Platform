8cee5e04ed04119c2f65e26c3ab8c5a9b7fa5cfd
園洛樱介 <1937009360@qq.com>
2025-02-22 15:14:36 +0800

    [fixed]修复nest-cli的sourceRoot造成项目启动问题

--- apps/backend/src/post/entities/post.entity.ts
+++ apps/backend/src/post/entities/post.entity.ts
@@ -2,6 +2,27 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Post {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  title: string;
+
+  @Field({ nullable: true })
+  slug?: string;
+
+  @Field({ nullable: true })
+  thumbnail?: string;
+
+  @Field()
+  content: string;
+
+  @Field(() => Boolean)
+  published: boolean;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
 }
--- apps/backend/src/post/post.resolver.ts
+++ apps/backend/src/post/post.resolver.ts
@@ -3,33 +3,16 @@ import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Inject } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Resolver(() => Post)
 export class PostResolver {
-  constructor(private readonly postService: PostService) {}
+  @Inject()
+  private prismaService: PrismaService;
 
-  @Mutation(() => Post)
-  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
-    return this.postService.create(createPostInput);
-  }
-
-  @Query(() => [Post], { name: 'post' })
-  findAll() {
-    return this.postService.findAll();
-  }
-
-  @Query(() => Post, { name: 'post' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.postService.findOne(id);
-  }
-
-  @Mutation(() => Post)
-  updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
-    return this.postService.update(updatePostInput.id, updatePostInput);
-  }
-
-  @Mutation(() => Post)
-  removePost(@Args('id', { type: () => Int }) id: number) {
-    return this.postService.remove(id);
+  @Query(() => [Post], { name: 'posts' })
+  async findAll() {
+    return await this.prismaService.post.findMany();
   }
 }
