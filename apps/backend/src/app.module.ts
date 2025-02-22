1aaac7cde5b3a69f0600af6a94663949438d8cde
園洛樱介 <1937009360@qq.com>
2025-02-23 00:03:38 +0800

    [add]like的entity

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 import { TagModule } from './tag/tag.module';
+import { LikeModule } from './like/like.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TagModule } from './tag/tag.module';
     UserModule,
     CommentModule,
     TagModule,
+    LikeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
